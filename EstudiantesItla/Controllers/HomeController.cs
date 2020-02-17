e72b23549a44b15abd7545944d8eca4d358faf06
EHeiliger <heiliger.eliet@hotmail.com>
2020-02-16 20:52:11 -0400

    initial commit

---
+++ EstudiantesItla/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EstudiantesItla.Models;
+
+namespace EstudiantesItla.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<ListaModeloTabla> Lst;
+            using (ITLAEntities1 db= new ITLAEntities1())
+            {
+                    Lst = (from d in db.Estudiantes
+                    select new ListaModeloTabla()
+                    {
+                        Nombre = d.Nombre,
+                        Apellido = d.Apellido,
+                        Carrera = d.Carrera,
+                        Matricula = d.Matricula,
+                        Status = d.Status
+                    }).ToList();
+            }
+            return View(Lst);
+        }
+
+        public ActionResult AddNuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GuardarNuevo(string Nombre,string Apellido, string Carrera)
+        {
+            using (ITLAEntities1 db = new ITLAEntities1())
+            {
+                var datos = new Estudiantes();
+                datos.Nombre = Nombre;
+                datos.Apellido = Apellido;
+                datos.Carrera = Carrera;
+                datos.Status = "t";
+                db.Estudiantes.Add(datos);
+                db.SaveChanges();
+            }
+
+            return Redirect("/home");
+            
+        }
+        [HttpPost]
+        public ActionResult Editar(int id)
+        {
+            ListaModeloTabla model = new ListaModeloTabla();
+            using (ITLAEntities1 db = new ITLAEntities1())
+            {
+                var Estudiante = db.Estudiantes.Find(id);
+                model.Nombre = Estudiante.Nombre;
+                model.Apellido = Estudiante.Apellido;
+                model.Carrera = Estudiante.Carrera;
+                model.Status = Estudiante.Status;
+                model.Matricula = id;
+            }
+
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public ActionResult GuardarCambio(int Matricula, string Apellido, string Nombre, string Status, string Carrera)
+        {
+            using (ITLAEntities1 db = new ITLAEntities1())
+            {
+                
+                var estudiante = db.Estudiantes.Find(Matricula);
+                estudiante.Nombre = Nombre;
+                estudiante.Apellido = Apellido;
+                estudiante.Carrera = Carrera;
+                estudiante.Status = Status;
+                db.Entry(estudiante).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Redirect("/home");
+        }
+        [HttpPost]
+        public ActionResult Eliminar(int id)
+        {
+            using (ITLAEntities1 db = new ITLAEntities1())
+            {
+                var estudiante = db.Estudiantes.Find(id);
+                db.Estudiantes.Remove(estudiante);
+                db.SaveChanges();
+            }
+
+            return Redirect("/home");
+        }
+    }
+    
+}
---
+++ EstudiantesItla/Models/ListaModeloTabla.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstudiantesItla.Models
+{
+    public class ListaModeloTabla
+    {
+        public int Matricula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Carrera { get; set; }
+        public string Status { get; set; }
+    }
+}
